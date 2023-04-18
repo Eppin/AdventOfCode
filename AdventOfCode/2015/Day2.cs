a9881e74b180fd4c9f1db57d029e83d0451b05ac
Eppin <96589679+Eppin@users.noreply.github.com>
2023-04-18 22:00:21 +0200

    Added day 2 of 2015 and easy reading of input data

--- AdventOfCode/2015/Day2.cs
+++ AdventOfCode/2015/Day2.cs
@@ -2,15 +2,54 @@ namespace AdventOfCode._2015;
 
 public class Day2 : Day
 {
+    public Day2() : base()
+    {
+    }
+
     public override string SolveA()
     {
-        throw new NotImplementedException();
+        var result = 0;
+        foreach (var str in SplitInput)
+        {
+            var split = str.Split('x');
+
+            var l = int.Parse(split[0]);
+            var w = int.Parse(split[1]);
+            var h = int.Parse(split[2]);
+
+            var area1 = l * w;
+            var area2 = w * h;
+            var area3 = h * l;
+
+            var areas = new[] { area1, area2, area3 };
+
+            result += areas.Select(a => 2 * a).Sum();
+            result += areas.Min();
+        }
+
+        return $"{result}";
     }
 
     public override string SolveB()
     {
-        throw new NotImplementedException();
-    }
+        var result = 0;
+        foreach (var str in SplitInput)
+        {
+            var split = str.Split('x');
 
-    public override string Input { get; }
+            var l = int.Parse(split[0]);
+            var w = int.Parse(split[1]);
+            var h = int.Parse(split[2]);
+
+            var wrap = new[] { l, w, h }
+                .Order()
+                .Take(2)
+                .Sum(i => i + i);
+
+            var ribbon = l * w * h;
+            result += wrap + ribbon;
+        }
+
+        return $"{result}";
+    }
 }
--- AdventOfCode/Day.cs
+++ AdventOfCode/Day.cs
@@ -1,8 +1,49 @@
 namespace AdventOfCode;
 
-public abstract class Day
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+public abstract partial class Day
 {
+    private readonly int _year;
+    private readonly int _day;
+
+    protected Day([CallerFilePath] string? filePath = null)
+    {
+        if (string.IsNullOrWhiteSpace(filePath))
+            throw new InvalidDataException("Caller file path can't be empty");
+
+        var regex = AdventOfCodeRegex();
+        var matches = regex.Match(filePath);
+
+        if (!matches.Success)
+            throw new DataMisalignedException($"Caller file path needs to contain year and day, current value [{filePath}]");
+        
+        var year = matches.Groups[1].Value;
+        var day = matches.Groups[2].Value;
+
+        if (!int.TryParse(year, out _year) || !int.TryParse(day, out _day))
+            throw new InvalidDataException($"Missing date [{year}/{day}]");
+    }
+
     public abstract string SolveA();
     public abstract string SolveB();
-    public abstract string Input { get; }
+    protected string Input => GetInput();
+    protected string[] SplitInput => GetSplitInput();
+
+    private string GetInput()
+    {
+        using var stream = typeof(Day).Assembly.GetManifestResourceStream($"{nameof(AdventOfCode)}._{_year}.Input.Day{_day}.txt");
+
+        using var reader = new StreamReader(stream ?? throw new InvalidOperationException());
+        return reader.ReadToEnd();
+    }
+
+    private string[] GetSplitInput()
+    {
+        return GetInput().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    [GeneratedRegex("AdventOfCode\\/(\\d{4})\\/Day(\\d{1,2})\\.")]
+    private static partial Regex AdventOfCodeRegex();
 }
