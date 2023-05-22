f4bb7351c5b69035b0428e7f5bd68454f3008af6
Eppin <96589679+Eppin@users.noreply.github.com>
2023-05-22 17:04:25 +0200

    Finished day 11

--- AdventOfCode/2015/Day11.cs
+++ AdventOfCode/2015/Day11.cs
@@ -8,32 +8,69 @@ public class Day11 : Day
 
     public override string SolveA()
     {
-        IEnumerable<char> a = Input;
-        int p = 0;
-        for (int i = 0; i < 20_000; i++)
-        {
-            (a, p) = Increment(a.ToList());
-            Console.WriteLine($"{string.Join("", a)}, {p}");
-        }
+        IList<char> input = Input.ToList();
 
-        return "";
+        return string.Join("", Solve(input));
     }
 
     public override string SolveB()
     {
-        throw new NotImplementedException();
+        IList<char> input = Input.ToList();
+        input = Solve(input);
+
+        return string.Join("", Solve(input));
     }
 
-    private static (IEnumerable<char>, int) Increment(List<char> input, int position = 0)
+    private static IList<char> Solve(IList<char> input)
     {
-        var x = input.Count - position - 1;
+        do
+        {
+            (input, _) = Increment(input);
+        } while (!Valid(input));
 
-        input[x] = IncrementChar(input[x]);
+        return input;
+    }
 
-        if (input[x] == 'a')
+    private static bool Valid(IList<char> input)
+    {
+        if (input.Any(s => s is 'i' or 'o' or 'l'))
+            return false;
+
+        var hasStraight = false;
+        var overlapping = 0;
+        var skipOverlapping = 0;
+
+        for (var i = 0; i < input.Count; i++)
+        {
+            if (!hasStraight && i + 2 < input.Count)
+            {
+                var next1 = (char)(input[i] + 1);
+                var next2 = (char)(input[i] + 2);
+
+                if (input[i + 1] == next1 && input[i + 2] == next2)
+                    hasStraight = true;
+            }
+
+            if (i < input.Count - 1 && i != skipOverlapping && input[i] == input[i + 1])
+            {
+                overlapping++;
+                skipOverlapping = i + 1;
+            }
+        }
+
+        return hasStraight && overlapping >= 2;
+    }
+
+    private static (IList<char>, int) Increment(IList<char> input, int position = 0)
+    {
+        var i = input.Count - position - 1;
+
+        input[i] = IncrementChar(input[i]);
+
+        if (input[i] == 'a')
             return Increment(input, ++position);
 
-        if (position > 0 && input[x + 1] == 'a')
+        if (position > 0 && input[i + 1] == 'a')
             position -= 1;
 
         return (input, position);
