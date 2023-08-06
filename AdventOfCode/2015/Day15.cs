e6b7be93fa989f4389782c58f37a29b454bf3615
Eppin <96589679+Eppin@users.noreply.github.com>
2023-08-06 15:35:28 +0200

    Beautified day 15

--- AdventOfCode/2015/Day15.cs
+++ AdventOfCode/2015/Day15.cs
@@ -5,8 +5,8 @@ using System.Text.RegularExpressions;
 
 public partial class Day15 : Day
 {
-    private int Ingredients;
-    private const int Teaspoons = 100;
+    private int _totalIngredients;
+    private const int TotalTeaspoons = 100;
 
     public Day15() : base()
     {
@@ -25,159 +25,36 @@ public partial class Day15 : Day
     private int Solve(bool isPartB)
     {
         var ingredients = Parse().ToList();
-        Ingredients = ingredients.Count;
+        _totalIngredients = ingredients.Count;
 
-        // var ingredients = 3;
-        // var teaspoons = 5;
-
-        var tot = 0;
-
-        // for (var i = 0; i <= Teaspoons; i++)
-        // {
-        //     for (var j = 0; j <= Teaspoons - i; j++)
-        //     {
-        //         for (var k = 0; k <= Teaspoons - i - j; k++)
-        //         {
-        //             // if(i + j + k == teaspoons && i != 0 && j != 0 && k != 0)
-        //             tot++;
-        //             Console.WriteLine($"{i},{j},{k} = {i + j + k == Teaspoons} {i != 0 && j != 0 && k != 0} {tot}");
-        //         }
-        //     }
-        // }
-
-        var xx = Nested2(Ingredients, Teaspoons, new List<int>());
-        var xg = xx.Where(x => x.Sum(g => g) == Teaspoons && x.All(g => g > 0));
-
-        var max = 0;
-        var maxPartB = 0;
-
-        foreach (var xa in xg)
-        {
-            // Console.WriteLine("Sum start:");
-            var s = Sum(xa, ingredients);
-
-            if (s.Item1 > max)
-                max = s.Item1;
-
-            if (s.Item2 == 500 && s.Item1 > maxPartB)
-                maxPartB = s.Item1;
-
-            // Console.WriteLine($"Tot{s.Item1}, cal: {s.Item2}");
-        }
+        var calculated = Nested(_totalIngredients, TotalTeaspoons, new List<int>())
+            .Where(x => x.Sum(g => g) == TotalTeaspoons && x.All(g => g > 0))
+            .Select(x => Sum(x, ingredients));
 
         return isPartB
-            ? maxPartB
-            : max;
+            ? calculated.Where(x => x.Calories == 500).Max(x => x.Score)
+            : calculated.Max(x => x.Score);
     }
 
-    private static int tot = 0;
-
-    private List<List<int>> Nested2(int ingredients, int teaspoons, List<int> x)
+    private static IEnumerable<List<int>> Nested(int ingredients, int teaspoons, List<int> x)
     {
         if (ingredients == 0)
             return new List<List<int>> { x };
 
         var results = new List<List<int>>();
-        for (var i = 0; i <= teaspoons; i++)
+        for (var i = 1; i <= teaspoons; i++)
         {
             var a = new List<int>(x) { i };
 
-            // if (a.Count == Ingredients)
-            // {
-            //     tot++;
-            //     Console.WriteLine($"Ingr: {ingredients}, teas: {i}, tot: {string.Join(",", a)}, tot2: {tot}");
-            // }
-
-            var ab = Nested2(ingredients - 1, teaspoons - i, a);
+            var ab = Nested(ingredients - 1, teaspoons - i, a);
             results.AddRange(ab);
         }
 
         return results;
     }
 
-    // Working example
-    // private void Nested2(int ingredients, int teaspoons, string x)
-    // {
-    //     if (ingredients == 0)
-    //         return;
-    //
-    //     for (var i = 0; i <= teaspoons; i++)
-    //     {
-    //         if ($"{x}{i}".Length == Ingredients)
-    //         {
-    //             tot++;
-    //             Console.WriteLine($"Ingr: {ingredients}, teas: {i}, tot: {x}{i}, tot2: {tot}");
-    //         }
-    //
-    //         Nested2(ingredients - 1, teaspoons - i, $"{x}{i}");
-    //     }
-    // }
-
-    // private void Nested(int ingredients, int teaspoons)
-    // {
-    //     if (ingredients == 0)
-    //     {
-    //         Console.WriteLine($"[{ingredients}]={teaspoons} <-- bottom");
-    //         return;
-    //     }
-    //
-    //     for (var i = 0; i < teaspoons; i++)
-    //     {
-    //         Console.WriteLine($"[{ingredients}]={i}");
-    //         Nested(ingredients - 1, teaspoons - i);
-    //     }
-    // }
-
-    // private void Nested(List<int> spread, int position, int teaspoon)
-    // {
-    // if (spread[position] == 1)
-    // {
-    //     spread[position - 1] = 1;
-    //     spread[position - 2] += 1;
-    //     spread[position] += teaspoon - spread.Sum();
-    // }
-    // else
-
-    // if (position == 0)
-    // {
-    //     if (spread.Last() == 1)
-    //         return;
-    //
-    //     position = spread.Count - 1;
-    // }
-
-    // spread[position] -= 1;
-    // spread[position - 1] += 1;
-    //
-    // if (spread.Contains(0))
-    //     return;
-    //
-    // Console.WriteLine(string.Join(",", spread));
-    //
-    // if (spread[position] == 1)
-    // {
-    //     spread[position - 2] += 1;
-    //     spread[position - 1] = 1;
-    //     spread[position] += teaspoon - spread.Sum();
-    //     
-    //     Console.WriteLine(string.Join(",", spread));
-    //     
-    //     Nested(spread, position, teaspoon);
-    // }
-    // else
-    // {
-    //     var pos = spread.Last() == 1 ? position - 2 : position;
-    //
-    //
-    //
-    //     Nested(spread, pos, teaspoon);
-    // }
-    // }
-
-    private (int, int) Sum(List<int> teaspoons, List<Ingredient> ingredients)
+    private static (int Score, int Calories) Sum(IEnumerable<int> teaspoons, List<Ingredient> ingredients)
     {
-        // Console.WriteLine($"Spoons: {string.Join(",", teaspoons)}, ingr: {String.Join(",", ingredients)}");
-
         var capacity = 0;
         var durability = 0;
         var flavor = 0;
@@ -187,23 +64,19 @@ public partial class Day15 : Day
         foreach (var (teaspoon, index) in teaspoons.Select((t, i) => (t, i)))
         {
             var ingredient = ingredients[index];
-            // foreach (var ingredient in ingredients)
-            // {
+
             capacity += ingredient.Capacity * teaspoon;
             durability += ingredient.Durability * teaspoon;
             flavor += ingredient.Flavor * teaspoon;
             texture += ingredient.Texture * teaspoon;
 
             calories += ingredient.Calories * teaspoon;
-
-            // yield return (capacity + durability + flavor + texture, calories);
-            // }
         }
 
         if (capacity < 0 || durability < 0 || flavor < 0 || texture < 0)
             return (0, calories);
 
-        return (Math.Max(capacity * durability * flavor * texture, 0), calories);
+        return (capacity * durability * flavor * texture, calories);
     }
 
     private IEnumerable<Ingredient> Parse()
