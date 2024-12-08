3216e6e8c7df1631750755209df103d82a2f0870
Eppin <96589679+Eppin@users.noreply.github.com>
2024-12-08 12:51:53 +0100

    Day 8 is finished
    Need to do some cleaning

--- AdventOfCode/2024/Day8.cs
+++ AdventOfCode/2024/Day8.cs
@@ -9,15 +9,17 @@ public class Day8 : Day
     }
 
     [Answer("14", Example, Data = "............{nl}........0...{nl}.....0......{nl}.......0....{nl}....0.......{nl}......A.....{nl}............{nl}............{nl}........A...{nl}.........A..{nl}............{nl}............{nl}")]
-    [Answer("", Regular)]
+    [Answer("379", Regular)]
     public override string SolveA()
     {
         return Solve().ToString();
     }
 
+    [Answer("9", Example, Data = "T.........{nl}...T......{nl}.T........{nl}..........{nl}..........{nl}..........{nl}..........{nl}..........{nl}..........{nl}..........")]
+    [Answer("", Regular)]
     public override string SolveB()
     {
-        throw new NotImplementedException();
+        return Solve().ToString();
     }
 
     private int Solve()
@@ -52,50 +54,74 @@ public class Day8 : Day
                     var diffX = c2.X - c1.X;
                     var diffY = c2.Y - c1.Y;
 
+                    var plus1 = true;
                     var point1 = new Point(c1.X + diffX, c1.Y + diffY);
+
                     if (point1 == c2)
+                    {
                         point1 = new Point(c1.X - diffX, c1.Y - diffY);
+                        plus1 = false;
+                    }
+
+                    result.Add(new Coordinate(c1, group.Key));
+                    result.Add(new Coordinate(point1, group.Key));
+
+                    while (point1.Y < input.Length && point1.X < input[0].Length && point1.Y >= 0 && point1.X >= 0)
+                    {
+                        if (plus1)
+                        {
+                            point1 = new Point(point1.X + diffX, point1.Y + diffY);
+                        }
+                        else
+                            point1 = new Point(point1.X - diffX, point1.Y - diffY);
+
+                        result.Add(new Coordinate(point1, group.Key));
+                    }
+
+                    ////
+
 
                     var point2 = new Point(c2.X + diffX, c2.Y + diffY);
-                    if (point1 == c1)
+                    if (point2 == c1)
                         point2 = new Point(c2.X - diffX, c2.Y - diffY);
 
-                    // Console.WriteLine($"{group.Key}: {c1.X},{c1.Y} vs {c2.X},{c2.Y} = {point1.X},{point1.Y} / {point2.X},{point2.Y}");
-                    result.Add(new Coordinate(point1, group.Key));
+
                     result.Add(new Coordinate(point2, group.Key));
+
+                    Console.WriteLine($"{group.Key}: {c1.X},{c1.Y} vs {c2.X},{c2.Y} = {point1.X},{point1.Y} / {point2.X},{point2.Y}");
                 }
             }
         }
 
         // Draw initial
-        // for (var y = 0; y < input.Length; y++)
-        // {
-        //     for (var x = 0; x < input[0].Length; x++)
-        //     {
-        //         var c = input[y][x];
-        //         Console.Write(c);
-        //     }
-        //
-        //     Console.WriteLine();
-        // }
+        for (var y = 0; y < input.Length; y++)
+        {
+            for (var x = 0; x < input[0].Length; x++)
+            {
+                var c = input[y][x];
+                Console.Write(c);
+            }
+        
+            Console.WriteLine();
+        }
 
-        // Console.WriteLine();
-        // Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
 
         // Draw new
-        // for (var y = 0; y < input.Length; y++)
-        // {
-        //     for (var x = 0; x < input[0].Length; x++)
-        //     {
-        //         var c = input[y][x];
-        //         if (result.Any(r => r.Point == new Point(x, y)))
-        //             c = '#';
-        //
-        //         Console.Write(c);
-        //     }
-        //
-        //     Console.WriteLine();
-        // }
+        for (var y = 0; y < input.Length; y++)
+        {
+            for (var x = 0; x < input[0].Length; x++)
+            {
+                var c = input[y][x];
+                if (result.Any(r => r.Point == new Point(x, y)))
+                    c = '#';
+        
+                Console.Write(c);
+            }
+        
+            Console.WriteLine();
+        }
 
         var xy = result
             .DistinctBy(c => c.Point)
