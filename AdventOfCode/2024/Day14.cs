1b092baecb6f9efadc9859cb72806bbb7094bd3e
Eppin <96589679+Eppin@users.noreply.github.com>
2024-12-14 22:11:15 +0100

    Beautified day 14 of 2024

--- AdventOfCode/2024/Day14.cs
+++ AdventOfCode/2024/Day14.cs
@@ -5,15 +5,13 @@ using System.Text;
 
 public partial class Day14 : Day
 {
-    // temp
-    private const int Width = 101; //11;
-    private const int Height = 103; //7;
+    private const int Width = 101;
+    private const int Height = 103;
 
     public Day14() : base()
     {
     }
 
-    [Answer("", Example, Data = "p=0,4 v=3,-3{nl}p=6,3 v=-1,-3{nl}p=10,3 v=-1,2{nl}p=2,0 v=2,-1{nl}p=0,0 v=1,3{nl}p=3,0 v=-2,-2{nl}p=7,6 v=-1,-3{nl}p=3,0 v=-1,-2{nl}p=9,3 v=2,3{nl}p=7,3 v=-1,2{nl}p=2,4 v=2,-3{nl}p=9,5 v=-3,-3")]
     [Answer("218619324", Regular)]
     public override string SolveA()
     {
@@ -21,65 +19,47 @@ public partial class Day14 : Day
 
         for (var i = 0; i < 100; i++)
         {
-            Console.WriteLine($"After {i + 1} second(s)");
-
             foreach (var robot in robots)
-            {
-                // Console.WriteLine($"B, Start: {robot.Position.X}, {robot.Position.Y}, Velocity: {robot.Velocity.X}, {robot.Velocity.Y} ");
                 Move(robot);
-            }
-
-            Console.WriteLine();
         }
 
-        // State
-        Console.WriteLine(Draw(robots, Width, Height));
-        Console.WriteLine();
-
         // Quadrant
+        const int halfWidth = Width / 2;
+        const int halfHeight = Height / 2;
 
-        var halfWidth = Width / 2;
-        var halfHeight = Height / 2;
-
-        // 0 -> halfWidth -1 == X
-        // 0 -> halfHeight -1 == Y
-
-        // halfWidth -> width == X
-        // 0 -> halfHeight -1 == Y
-
-        // halfWidth -> width == X
-        // 0 -> halfHeight -1 == Y
-
-        var q1 = 0;
-        var q2 = 0;
-        var q3 = 0;
-        var q4 = 0;
-
-        Console.WriteLine($"{new Point(0, 0)} => {new Point(halfWidth - 1, halfHeight - 1)}");
-        Console.WriteLine($"{new Point(0, halfHeight + 1)} => {new Point(halfWidth - 1, Height - 1)}");
-
-        Console.WriteLine($"{new Point(halfWidth + 1, 0)} => {new Point(Width - 1, halfHeight - 1)}");
-        Console.WriteLine($"{new Point(halfWidth + 1, halfHeight + 1)} => {new Point(Width - 1, Height - 1)}");
+        var quadrant1 = 0;
+        var quadrant2 = 0;
+        var quadrant3 = 0;
+        var quadrant4 = 0;
 
         foreach (var robot in robots)
         {
-            if (robot.Position.X >= 0 && robot.Position.X <= halfWidth - 1 && robot.Position.Y >= 0 && robot.Position.Y <= halfHeight - 1)
-                q1++;
+            switch (robot.Position.X)
+            {
+                case >= 0 and <= halfWidth - 1 when robot.Position.Y is >= 0 and <= halfHeight - 1:
+                    quadrant1++;
+                    break;
 
-            if (robot.Position.X >= 0 && robot.Position.X <= halfWidth - 1 && robot.Position.Y >= halfHeight + 1 && robot.Position.Y <= Height - 1)
-                q2++;
+                case >= halfWidth + 1 and <= Width - 1 when robot.Position.Y is >= halfHeight + 1 and <= Height - 1:
+                    quadrant4++;
+                    break;
+            }
 
-            if (robot.Position.X >= halfWidth + 1 && robot.Position.X <= Width - 1 && robot.Position.Y >= 0 && robot.Position.Y <= halfHeight - 1)
-                q3++;
+            switch (robot.Position.X)
+            {
+                case >= 0 and <= halfWidth - 1 when robot.Position.Y is >= halfHeight + 1 and <= Height - 1:
+                    quadrant2++;
+                    break;
 
-            if (robot.Position.X >= halfWidth + 1 && robot.Position.X <= Width - 1 && robot.Position.Y >= halfHeight + 1 && robot.Position.Y <= Height - 1)
-                q4++;
+                case >= halfWidth + 1 and <= Width - 1 when robot.Position.Y is >= 0 and <= halfHeight - 1:
+                    quadrant3++;
+                    break;
+            }
         }
 
-        return (q1 * q2 * q3 * q4).ToString();
+        return (quadrant1 * quadrant2 * quadrant3 * quadrant4).ToString();
     }
 
-    [Answer("", Example, Data = "p=0,4 v=3,-3{nl}p=6,3 v=-1,-3{nl}p=10,3 v=-1,2{nl}p=2,0 v=2,-1{nl}p=0,0 v=1,3{nl}p=3,0 v=-2,-2{nl}p=7,6 v=-1,-3{nl}p=3,0 v=-1,-2{nl}p=9,3 v=2,3{nl}p=7,3 v=-1,2{nl}p=2,4 v=2,-3{nl}p=9,5 v=-3,-3")]
     [Answer("6446", Regular)]
     public override string SolveB()
     {
@@ -87,18 +67,13 @@ public partial class Day14 : Day
 
         for (var i = 0; i < Height * Width; i++)
         {
-            Console.WriteLine($"After {i + 1} second(s)");
-
             foreach (var robot in robots)
-            {
-                // Console.WriteLine($"B, Start: {robot.Position.X}, {robot.Position.Y}, Velocity: {robot.Velocity.X}, {robot.Velocity.Y} ");
                 Move(robot);
-            }
 
-            var str = $"{i + 1}" + Environment.NewLine + Draw(robots, Width, Height) + Environment.NewLine;
-            // Console.WriteLine(str);
-            File.AppendAllText("./trees.txt", str);
-            Console.WriteLine();
+            // Found these string by forcing all grids to a file and search for the christmas tree
+            var draw = Draw(robots, Width, Height);
+            if (draw.Contains("1111111111111111111111111111111"))
+                return (i + 1).ToString();
         }
 
         return 0.ToString();
@@ -106,34 +81,19 @@ public partial class Day14 : Day
 
     private static void Move(Robot robot)
     {
-        var velocityX = robot.Velocity.X;
-        var velocityY = robot.Velocity.Y;
+        robot.Position = (robot.Position.X + robot.Velocity.X) switch
+        {
+            < 0 => robot.Position with { X = Width + robot.Position.X + robot.Velocity.X },
+            >= Width => robot.Position with { X = robot.Position.X + robot.Velocity.X - Width },
+            _ => robot.Position with { X = robot.Position.X + robot.Velocity.X }
+        };
 
-        if (robot.Position.X + velocityX < 0) // Jump left of the grid
+        robot.Position = (robot.Position.Y + robot.Velocity.Y) switch
         {
-            robot.Position = robot.Position with { X = Width + (robot.Position.X + velocityX) };
-        }
-        else if (robot.Position.X + velocityX >= Width) // Jump right of the grid
-        {
-            robot.Position = robot.Position with { X = robot.Position.X + velocityX - Width };
-        }
-        else // Default change
-        {
-            robot.Position = robot.Position with { X = robot.Position.X + velocityX };
-        }
-
-        if (robot.Position.Y + velocityY < 0) // Jump at top of the grid
-        {
-            robot.Position = robot.Position with { Y = Height + (robot.Position.Y + velocityY) };
-        }
-        else if (robot.Position.Y + velocityY >= Height) // Jump at bottom of the grid
-        {
-            robot.Position = robot.Position with { Y = robot.Position.Y + velocityY - Height };
-        }
-        else // Default change
-        {
-            robot.Position = robot.Position with { Y = robot.Position.Y + velocityY };
-        }
+            < 0 => robot.Position with { Y = Height + robot.Position.Y + robot.Velocity.Y },
+            >= Height => robot.Position with { Y = robot.Position.Y + robot.Velocity.Y - Height },
+            _ => robot.Position with { Y = robot.Position.Y + robot.Velocity.Y }
+        };
     }
 
     private static string Draw(List<Robot> robots, int width, int height)
@@ -147,12 +107,8 @@ public partial class Day14 : Day
         for (var y = 0; y < grid.MaxY; y++)
         {
             for (var x = 0; x < grid.MaxX; x++)
-            {
                 sb.Append(grid[x, y]);
-                // Console.Write(grid[x, y]);
-            }
 
-            // Console.WriteLine();
             sb.AppendLine();
         }
 
