9a7ce9823e7bf925025f33d6e37308aa9a4c3d8b
Eppin <96589679+Eppin@users.noreply.github.com>
2024-12-18 18:43:37 +0100

    Adding Dijkstra algoritm

---
+++ AdventOfCode/Utils/Dijkstra.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Utils;
+
+using Coordinate = Coordinate<int>;
+
+public class Dijkstra<TNode> where TNode : notnull
+{
+    private List<TNode> _unvisited;
+    private List<TNode> _visited;
+    private Dictionary<TNode, int> _distances;
+
+    public Dijkstra(List<TNode> nodes, TNode start)
+    {
+        _unvisited = nodes;
+        _visited = [start];
+        _distances = new Dictionary<TNode, int> { { start, 0 } };
+    }
+
+    public void ShortestPath(TNode end, Func<TNode, IEnumerable<TNode>> neighbours, Func<TNode, int> distance)
+    {
+        var queue = new Queue<TNode>();
+        queue.Enqueue(_visited[0]);
+
+        while (queue.TryDequeue(out var node))
+        {
+            foreach (var n in neighbours.Invoke(node))
+            {
+                distance(n);
+            }
+        }
+    }
+
+
+}
