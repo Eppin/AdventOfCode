1314b880841652585df1e551f0af323a00b83a7c
Eppin <96589679+Eppin@users.noreply.github.com>
2024-12-22 13:23:12 +0100

    Finished day 22 of 2024

--- AdventOfCode/2024/Day22.cs
+++ AdventOfCode/2024/Day22.cs
@@ -10,16 +10,6 @@ public class Day22 : Day
     [Answer("13429191512", Regular)]
     public override string SolveA()
     {
-        return Solve().ToString();
-    }
-
-    public override string SolveB()
-    {
-        throw new NotImplementedException();
-    }
-
-    private long Solve()
-    {
         var secrets = Parse();
         var total = 0L;
 
@@ -32,7 +22,55 @@ public class Day22 : Day
             total += tmp;
         }
 
-        return total;
+        return total.ToString();
+    }
+
+    [Answer("23", Example, Data = "1{nl}2{nl}3{nl}2024")]
+    [Answer("1582", Regular)]
+    public override string SolveB()
+    {
+        var secrets = Parse();
+        
+        // Dictionary with changes as key, nested dictionary with secret as key and price as value
+        var changes = new Dictionary<string, Dictionary<long, long>>();
+
+        foreach (var secret in secrets)
+        {
+            var tmp = secret;
+            var price = tmp % 10;
+
+            long? change1 = null;
+            long? change2 = null;
+            long? change3 = null;
+
+            for (var i = 0; i < 1999; i++)
+            {
+                tmp = Next(tmp);
+
+                var tmpPrice = tmp % 10;
+                var change = tmp % 10 - price;
+                price = tmpPrice;
+
+                if (change1 != null && change2 != null && change3 != null)
+                {
+                    var changeStr = $"{change1.Value},{change2.Value},{change3.Value},{change}";
+                    if (!changes.TryAdd(changeStr, new Dictionary<long, long> { [secret] = tmpPrice }))
+                    {
+                        if (!changes[changeStr].ContainsKey(secret))
+                            changes[changeStr].Add(secret, tmpPrice);
+                    }
+                }
+
+                change1 = change2;
+                change2 = change3;
+                change3 = change;
+            }
+        }
+
+        return changes
+            .MaxBy(x => x.Value.Sum(s => s.Value)).Value
+            .Sum(s => s.Value)
+            .ToString();
     }
 
     private static long Next(long secret)
