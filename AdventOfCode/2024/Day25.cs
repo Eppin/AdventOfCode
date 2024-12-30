030980ea30b6f08c74144ac8199bbb79a2767a2f
Eppin <96589679+Eppin@users.noreply.github.com>
2024-12-30 16:36:54 +0100

    Finished day 25, part 1 of 2024

---
+++ AdventOfCode/2024/Day25.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace AdventOfCode._2024;
+
+public class Day25 : Day
+{
+    public Day25() : base()
+    {
+    }
+
+    [Answer("3", Example, Data = "#####{nl}.####{nl}.####{nl}.####{nl}.#.#.{nl}.#...{nl}.....{nl}{nl}#####{nl}##.##{nl}.#.##{nl}...##{nl}...#.{nl}...#.{nl}.....{nl}{nl}.....{nl}#....{nl}#....{nl}#...#{nl}#.#.#{nl}#.###{nl}#####{nl}{nl}.....{nl}.....{nl}#.#..{nl}###..{nl}###.#{nl}###.#{nl}#####{nl}{nl}.....{nl}.....{nl}.....{nl}#....{nl}#.#..{nl}#.#.#{nl}#####")]
+    [Answer("3116", Regular)]
+    public override string SolveA()
+    {
+        var (locks, keys) = Parse();
+        var count = 0;
+
+        foreach (var @lock in locks)
+        {
+            foreach (var key in keys)
+            {
+                var fail = false;
+                for (var i = 0; i < 5; i++)
+                {
+                    if (@lock[i] + key[i] > 5)
+                    {
+                        fail = true;
+                        break;
+                    }
+                }
+
+                if (!fail) count++;
+            }
+        }
+
+        return count.ToString();
+    }
+
+    public override string SolveB()
+    {
+        throw new NotImplementedException();
+    }
+
+    private (List<List<int>> Locks, List<List<int>> Keys) Parse()
+    {
+        var locks = new List<List<int>>();
+        var keys = new List<List<int>>();
+        var tmp = new List<string>();
+
+        string? first;
+        string? last;
+
+        foreach (var line in GetSplitInput(false))
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                first = tmp.First();
+                last = tmp.Last();
+                if (first.ToCharArray().All(a => a is '#') && last.ToCharArray().All(a => a is '.'))
+                    locks.Add(AddToList(tmp));
+                else
+                    keys.Add(AddToList(tmp));
+
+                tmp.Clear();
+                continue;
+            }
+
+            tmp.Add(line);
+        }
+
+        // Process last one...
+        first = tmp.First();
+        last = tmp.Last();
+        if (first.ToCharArray().All(a => a is '#') && last.ToCharArray().All(a => a is '.'))
+            locks.Add(AddToList(tmp));
+        else
+            keys.Add(AddToList(tmp));
+
+        tmp.Clear();
+
+        return (locks, keys);
+    }
+
+    private static List<int> AddToList(List<string> tmp)
+    {
+        var array = tmp.Select(s => s.ToCharArray()).ToArray();
+        var grid = new Grid<char>(array);
+
+        var list = new List<int>();
+        for (var x = 0; x < grid.MaxX; x++)
+        {
+            var count = -1;
+            for (var y = 0; y < grid.MaxY; y++)
+            {
+                if (grid[x, y] is '#')
+                    count++;
+            }
+
+            list.Add(count);
+        }
+
+        return list;
+    }
+}
