32601f2c0b8fe2320e3923d453fe03a9c0f9576d
Eppin <96589679+Eppin@users.noreply.github.com>
2024-12-31 14:41:25 +0100

    Finished day 23 part 2 (#11)

--- AdventOfCode/2024/Day23.cs
+++ AdventOfCode/2024/Day23.cs
@@ -13,29 +13,27 @@ public class Day23 : Day
         var items = Parse();
         var queue = new Queue<(Node Node, string End, int Depth, HashSet<Node> Visited)>();
 
-        foreach (var node in items)//.Skip(1).Take(1))
-        {
+        foreach (var node in items)
             queue.Enqueue((node, node.A, 0, [node]));
-            //queue.Enqueue((node, node.B, 0));
-        }
 
         var list = new HashSet<Unique>();
 
         while (queue.Count > 0)
         {
             var (node, end, depth, visited) = queue.Dequeue();
-            //Console.WriteLine($"N:{node}, E:{end}, D:{depth}, V:{string.Join(',', visited)}");
 
             if (depth == 2)
             {
                 if (node.A == end || node.B == end)
                 {
-                    //Console.WriteLine("OK!");
+                    var uniques = visited
+                        .Select(x => new List<string> { x.A, x.B })
+                        .SelectMany(x => x)
+                        .Distinct()
+                        .Order()
+                        .ToList();
 
-                    var k = visited.Select(x => new List<string> { x.A, x.B }).SelectMany(x => x).Distinct().Order().ToList();
-                    //Console.WriteLine($"N:{node}, E:{end}, D:{depth}, V:{string.Join(',', k)}"); // {string.Join(',', visited)}
-                    list.Add(new Unique(k[0], k[1], k[2]));
-                    //Console.WriteLine($"ADD: {string.Join(',', k)}");
+                    list.Add(new Unique(uniques[0], uniques[1], uniques[2]));
                 }
 
                 continue;
@@ -47,43 +45,61 @@ public class Day23 : Day
             }
         }
 
-        var count = 0;
-        foreach (var k in list.Distinct().Where(t => t.A.StartsWith('t') || t.B.StartsWith('t') || t.C.StartsWith('t')))//.OrderBy(x => x[0]))
-        {
-            Console.WriteLine($"V:{string.Join(',', k)}");
-            count++;
-        }
-
-        return count.ToString();
+        return list
+            .Distinct()
+            .Count(t => t.A.StartsWith('t') || t.B.StartsWith('t') || t.C.StartsWith('t'))
+            .ToString();
     }
 
-    private static void Loop(List<Node> items, Node node, string end, int depth)
-    {
-
-
-        Console.WriteLine($"N:{node}, E:{end}, D:{depth}");
-
-        if (depth == 3)
-        {
-            if (node.A == end)
-                Console.WriteLine("OK!");
-            else
-            {
-                Console.WriteLine("FAIL!!");
-                return;
-            }
-        }
-
-        foreach (var next in items.Where(i => i != node && (i.A == node.B || i.B == node.B)))
-        {
-            //Loop(items, a, b, depth + 1);
-            Loop(items, next, end, depth + 1);
-        }
-    }
-
+    [Answer("co,de,ka,ta", Example, Data = "kh-tc{nl}qp-kh{nl}de-cg{nl}ka-co{nl}yn-aq{nl}qp-ub{nl}cg-tb{nl}vc-aq{nl}tb-ka{nl}wh-tc{nl}yn-cg{nl}kh-ub{nl}ta-co{nl}de-co{nl}tc-td{nl}tb-wq{nl}wh-td{nl}ta-ka{nl}td-qp{nl}aq-cg{nl}wq-ub{nl}ub-vc{nl}de-ta{nl}wq-aq{nl}wq-vc{nl}wh-yn{nl}ka-de{nl}kh-ta{nl}co-tc{nl}wh-qp{nl}tb-vc{nl}td-yn")]
+    [Answer("af,aq,ck,ee,fb,it,kg,of,ol,rt,sc,vk,zh", Regular)]
     public override string SolveB()
     {
-        throw new NotImplementedException();
+        var items = Parse();
+        var combinations = new Dictionary<string, List<string>>();
+
+        foreach (var item in items)
+        {
+            if (!combinations.TryAdd(item.A, [item.B]))
+                combinations[item.A].Add(item.B);
+
+            if (!combinations.TryAdd(item.B, [item.A]))
+                combinations[item.B].Add(item.A);
+        }
+
+        var result = new List<string>();
+
+        foreach (var (key, value) in combinations)
+        {
+            var keys = new HashSet<string> { key };
+            var neighbours = combinations.Where(d => value.Contains(d.Key));
+
+            foreach (var c2 in neighbours)
+            {
+                var v = c2.Value.Where(value.Contains);
+                foreach (var y in v)
+                    keys.Add(y);
+            }
+
+            var valid = false;
+            foreach (var key2 in keys)
+            {
+                if (!keys.All(h => combinations[key2].Contains(h) || h == key2))
+                {
+                    valid = false;
+                    break;
+                }
+
+                valid = true;
+            }
+
+            if (!valid || keys.Count <= result.Count) continue;
+
+            result.Clear();
+            result.AddRange(keys);
+        }
+
+        return string.Join(',', result.Order());
     }
 
     private List<Node> Parse()
@@ -98,6 +114,5 @@ public class Day23 : Day
     }
 
     private record struct Node(string A, string B);
-
     private record struct Unique(string A, string B, string C);
 }
