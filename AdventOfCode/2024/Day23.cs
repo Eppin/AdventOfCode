a509817b29e3f779ee040bb3f31575a53acc299d
Eppin <96589679+Eppin@users.noreply.github.com>
2024-12-31 14:40:44 +0100

    Refactored day 23, part 1

--- AdventOfCode/2024/Day23.cs
+++ AdventOfCode/2024/Day23.cs
@@ -6,9 +6,49 @@ public class Day23 : Day
     {
     }
 
+    [Answer("7", Example, Data = "kh-tc{nl}qp-kh{nl}de-cg{nl}ka-co{nl}yn-aq{nl}qp-ub{nl}cg-tb{nl}vc-aq{nl}tb-ka{nl}wh-tc{nl}yn-cg{nl}kh-ub{nl}ta-co{nl}de-co{nl}tc-td{nl}tb-wq{nl}wh-td{nl}ta-ka{nl}td-qp{nl}aq-cg{nl}wq-ub{nl}ub-vc{nl}de-ta{nl}wq-aq{nl}wq-vc{nl}wh-yn{nl}ka-de{nl}kh-ta{nl}co-tc{nl}wh-qp{nl}tb-vc{nl}td-yn")]
+    [Answer("1419", Regular)]
     public override string SolveA()
     {
-        throw new NotImplementedException();
+        var items = Parse();
+        var queue = new Queue<(Node Node, string End, int Depth, HashSet<Node> Visited)>();
+
+        foreach (var node in items)
+            queue.Enqueue((node, node.A, 0, [node]));
+
+        var list = new HashSet<Unique>();
+
+        while (queue.Count > 0)
+        {
+            var (node, end, depth, visited) = queue.Dequeue();
+
+            if (depth == 2)
+            {
+                if (node.A == end || node.B == end)
+                {
+                    var uniques = visited
+                        .Select(x => new List<string> { x.A, x.B })
+                        .SelectMany(x => x)
+                        .Distinct()
+                        .Order()
+                        .ToList();
+
+                    list.Add(new Unique(uniques[0], uniques[1], uniques[2]));
+                }
+
+                continue;
+            }
+
+            foreach (var next in items.Where(i => !visited.Contains(i) && (i.A == node.B || i.B == node.B)))
+            {
+                queue.Enqueue((next, end, depth + 1, [.. visited, next]));
+            }
+        }
+
+        return list
+            .Distinct()
+            .Count(t => t.A.StartsWith('t') || t.B.StartsWith('t') || t.C.StartsWith('t'))
+            .ToString();
     }
 
     [Answer("co,de,ka,ta", Example, Data = "kh-tc{nl}qp-kh{nl}de-cg{nl}ka-co{nl}yn-aq{nl}qp-ub{nl}cg-tb{nl}vc-aq{nl}tb-ka{nl}wh-tc{nl}yn-cg{nl}kh-ub{nl}ta-co{nl}de-co{nl}tc-td{nl}tb-wq{nl}wh-td{nl}ta-ka{nl}td-qp{nl}aq-cg{nl}wq-ub{nl}ub-vc{nl}de-ta{nl}wq-aq{nl}wq-vc{nl}wh-yn{nl}ka-de{nl}kh-ta{nl}co-tc{nl}wh-qp{nl}tb-vc{nl}td-yn")]
@@ -20,11 +60,11 @@ public class Day23 : Day
 
         foreach (var item in items)
         {
-            if (!combinations.TryAdd(item[0], [item[1]]))
-                combinations[item[0]].Add(item[1]);
+            if (!combinations.TryAdd(item.A, [item.B]))
+                combinations[item.A].Add(item.B);
 
-            if (!combinations.TryAdd(item[1], [item[0]]))
-                combinations[item[1]].Add(item[0]);
+            if (!combinations.TryAdd(item.B, [item.A]))
+                combinations[item.B].Add(item.A);
         }
 
         var result = new List<string>();
@@ -62,10 +102,17 @@ public class Day23 : Day
         return string.Join(',', result.Order());
     }
 
-    private string[][] Parse()
+    private List<Node> Parse()
     {
         return GetSplitInput()
-            .Select(s => s.Split('-'))
-            .ToArray();
+            .Select(s =>
+            {
+                var split = s.Split('-');
+                return new Node(split[0], split[1]);
+            })
+            .ToList();
     }
+
+    private record struct Node(string A, string B);
+    private record struct Unique(string A, string B, string C);
 }
