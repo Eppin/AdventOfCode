136a1aa59282242bda3632c55a2c35aaba088df9
Eppin <96589679+Eppin@users.noreply.github.com>
2025-01-08 17:38:27 +0100

    Finished day 24 of 2024 (#12)

--- AdventOfCode/2024/Day24.cs
+++ AdventOfCode/2024/Day24.cs
@@ -12,6 +12,53 @@ public partial class Day24 : Day
     {
         var (inputs, wires) = Parse();
 
+        VisitWires(wires, inputs);
+
+        return GetInt64(inputs, i => i.Key.StartsWith('z')).Numeric.ToString();
+    }
+
+    [Answer("z00,z01,z02,z05", Example, Data = "x00: 0{nl}x01: 1{nl}x02: 0{nl}x03: 1{nl}x04: 0{nl}x05: 1{nl}y00: 0{nl}y01: 0{nl}y02: 1{nl}y03: 1{nl}y04: 0{nl}y05: 1{nl}{nl}x00 AND y00 -> z05{nl}x01 AND y01 -> z02{nl}x02 AND y02 -> z01{nl}x03 AND y03 -> z03{nl}x04 AND y04 -> z04{nl}x05 AND y05 -> z00")]
+    [Answer("", Regular)]
+    public override string SolveB()
+    {
+        var (_, wires) = Parse();
+        
+        var falseWires = new List<string>();
+        
+        var outputWires = wires
+            .Select(w => w.Out)
+            .Where(w => w.StartsWith('z'))
+            .ToList();
+        
+        // Proper order of gates are:
+        // AND -> OR
+        // XOR -> AND
+        // But different ruling for inputs and outputs 
+        foreach (var wire in wires)
+        {
+            // starting wires should be followed by OR if AND, and by AND if XOR, except for the first one
+            if ((wire.In1.StartsWith('x') || wire.In2.StartsWith('x')) && !wire.In1.Contains("00") && !wire.In2.Contains("00"))
+                foreach (var secondWire in wires)
+                {
+                    if (wire.Out != secondWire.In1 && wire.Out != secondWire.In2) continue;
+                    if ((wire.Gate == Gate.And && secondWire.Gate == Gate.And) || (wire.Gate == Gate.Xor && secondWire.Gate == Gate.Or))
+                        falseWires.Add(wire.Out);
+                }
+
+            // wires in the middle should not have XOR operators
+            if (!wire.In1.StartsWith('x') && !wire.In2.StartsWith('x') && !wire.Out.StartsWith('z') && wire.Gate == Gate.Xor)
+                falseWires.Add(wire.Out);
+
+            // wires at the end should always have XOR operators, except for the last one
+            if (outputWires.Contains(wire.Out) && !wire.Out.Equals($"z{outputWires.Count - 1}") && wire.Gate != Gate.Xor)
+                falseWires.Add(wire.Out);
+        }
+        
+        return string.Join(",", falseWires.Order());
+    }
+
+    private static void VisitWires(List<Wire> wires, Dictionary<string, int> inputs)
+    {
         var visited = new HashSet<Wire>();
 
         while (true)
@@ -19,7 +66,13 @@ public partial class Day24 : Day
             if (visited.Count == wires.Count)
                 break;
 
-            foreach (var wire in wires.Where(w => !visited.Contains(w)))
+            var availableWires = wires
+                .Where(w => !visited.Contains(w) && inputs.ContainsKey(w.In1) && inputs.ContainsKey(w.In2))
+                .ToList();
+
+            if (availableWires.Count == 0) return;
+
+            foreach (var wire in availableWires)
             {
                 if (!inputs.TryGetValue(wire.In1, out var in1) || !inputs.TryGetValue(wire.In2, out var in2))
                     continue;
@@ -30,14 +83,17 @@ public partial class Day24 : Day
                 visited.Add(wire);
             }
         }
+    }
 
+    private static (long Numeric, string Bytes) GetInt64(Dictionary<string, int> inputs, Func<KeyValuePair<string, int>, bool> predicate)
+    {
         var z = inputs
-            .Where(i => i.Key.StartsWith('z'))
+            .Where(predicate)
             .OrderByDescending(i => i.Key)
             .Select(z => z.Value);
 
         var zz = string.Join("", z);
-        return Convert.ToInt64(zz, 2).ToString();
+        return (Convert.ToInt64(zz, 2), zz);
     }
 
     private static int Calculate(int in1, int in2, Gate gate)
@@ -49,11 +105,6 @@ public partial class Day24 : Day
             Gate.And => in1 & in2,
             _ => throw new ArgumentOutOfRangeException(nameof(gate), gate, null)
         };
-    }
-
-    public override string SolveB()
-    {
-        throw new NotImplementedException();
     }
 
     private (Dictionary<string, int> Inputs, List<Wire> Wires) Parse()
